5133fcadc2f3eb00fc65383b8a63b334006f3999
Christopher <10642047@my.uvu.edu>
2020-10-27 12:40:23 -0600

    Lesson 1.2 - Pedal to the Metal (Create With Code)
    1. Introduction
    - All objects are made of components
    - Vehicle will move forward through script component written in C#
    
    2. Create and apply your first script
    - Created Scripts folder in the Course Library to hold the scripts for the game
    - Created the C# file named PlayerController
    - Learned that the naming convention for scripts (use capitol letters with no spaces)
    - Dragged the PlayerController Script to the Vehicle
    - Learned how to remove components
    
    3. Add a comment in the Update() method
    - Double clicking opens scripts
    - Comments in C# use double slashes //
    - Methods have () then {} around the block
    - Added a devolopment comment in the Update() function.
    
    4. Give the vehicle a forward motion
    - Added the transform.Translate(0, 0, 1); to Update() and it will move every time screen is updated.
    - transform is the component
    - Translate is a method
    - Finished line off with a ;
    - Car now flies forward down the road.
    
    5. Use a Vector3 to move forward
    - Amended out code to "Make our lives a lot easier" by changing the coordinates to a Vector3
    - Vector3.forward is shorthand for (0, 0, 1)
    
    6. Customize the vehicle’s speed
    - Added the Time.deltaTime that makes it not update every frame but linearly over time.
    - Added 20 as a scaler to make it go faster
    
    7. Add RigidBody components to objects
    - Added a RigidBody to the Obstacle
    - Added a Rigidbody to the Vehicle
    - They both use physics, mass, and gravity now.
    - Increased the masses of the vehicle and obstacle
    
    8. Duplicate and position the obstacles
    - Added seven more boxes by using ctrl-d

---
+++ create-with-code/Unit 1 - Player Control/Prototype-1/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Forward
+        //transform.Translate(0, 0, 1); //Old Code uses coordinates instead of a Vector3
+        transform.Translate(Time.deltaTime * 20 * Vector3.forward);
+    }
+}
