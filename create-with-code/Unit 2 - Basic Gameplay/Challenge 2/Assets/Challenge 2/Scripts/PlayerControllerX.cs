80517f5343844acdbb09750b12ecd70d5932364e
Christopher <10642047@my.uvu.edu>
2020-10-27 17:31:09 -0600

    Challenge 2 - Play Fetch (Create With Code)
    - Created the Challenge 2 Unity Project
    - Imported the Challenge 2 asset pack from the unity store
    - Fixed that the dogs that were spawning at the top of the screen
    - Fixed that the player that was spawning green balls instead of dogs
    - Fixed that the balls being destroyed if anywhere near the dog
    - Fixed that Nothing was being destroyed off screen
    - Fixed that there was only one type of ball is being spawned
    - Fixed that the spawn interval is always the same
    - Fixed that player could “spam” the space-bar key

---
+++ create-with-code/Unit 2 - Basic Gameplay/Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public int holdingValue = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holdingValue == 0)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                holdingValue = 100;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+        }
+        else
+        {
+            holdingValue -= 1;
+        }
+
+    }
+}
