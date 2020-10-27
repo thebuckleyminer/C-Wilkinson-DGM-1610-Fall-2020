82488bfc657847c5abd81c289caeab405940f654
Christopher <10642047@my.uvu.edu>
2020-10-27 16:22:04 -0600

    Lesson 2.2 - Food Flight (Create With Code)
    1. Introduction
    - When the user presses the space-bar it will launch the food
    - We are going to turn the object into a reusable prefab
    
    2. Make the projectile fly forwards
    - Created the MoveForward C# script
    - Added the script to the bananas
    - Created a public float variable for speed and set it to 40.0f
    - Added a transform.Translate with the time scale to move forward
    
    3. Make the projectile into a prefab
    - Created a Prefabs folder in the Assets folder
    - Added the Banana as a new original prefab
    - Blue Cube denotes a prefab
    - Made a new public GameObject projectilePrefab variable
    - Dragged the banana to the projectilePrefab variable spot
    
    4. Test for space-bar press
    - Wrote an if statement in Update() to check for the space-bar
    if (Input.GetKeyDown(KeyCode.Space)){}
    - This checks for the space-bar is pressed down
    - Added a comment of where we are going to add the launching code
    
    5. Launch projectile on space-bar press
    - Coded the Instantiate() to make a new instance of the projectile prefab
    - Used the projectilePrefab, transform.position, projectilePrefab.transform.rotation in instantiate()
    - Bananas now launch when the space bar is pressed 
    
    6. Make animals into prefabs
    - Rotated all eight animals on the Y axis 180 degrees
    - Added the Move forward to all eight animals
    - Added different individual speeds to the eight animals
    - Made each of the eight animals to their own  original prefab
    
    7. Destroy projectiles off screen
    - Created the DestroyOutOfBounds C# script to do as the name suggests
    - Added the DestroyOutOfBounds script to the projectile bananas
    - Used an if statement to see if bananas are outside of the destroy boundary we set
    - Found use of Destroy() to get rid of our object
    - Created a variable for the top bound: private float topBound = 30.0f;
    - Overrided our prefab so all of the new food generated from pressing the space-bar gets destroyed when it goes to far too.
    
    8. Destroy animals off screen
    - Added the destroy out of bounds script to all eight animals
    - Created the lowerBound float variable
    - Used an else if at the end of the if to check for the min value
    - Set the logic so the animals will be destroyed if they cross the min threshold
    - Overrided all eight of the animal prefabs to have these properties

--- create-with-code/Unit 2 - Basic Gameplay/Prototype-2/Assets/Scripts/PlayerController.cs
+++ create-with-code/Unit 2 - Basic Gameplay/Prototype-2/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
+    public GameObject projectilePrefab;
     // Update is called once per frame
     void Update()
     {
@@ -22,5 +23,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(horizontalInput * Time.deltaTime * speed * Vector3.right);
+        
+        //Launches the banana
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
