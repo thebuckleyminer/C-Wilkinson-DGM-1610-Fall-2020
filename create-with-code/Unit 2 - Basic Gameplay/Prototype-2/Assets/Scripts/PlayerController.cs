d83f3bb9c511a861e403d1fd22a1dd474088fb03
Christopher <10642047@my.uvu.edu>
2020-10-27 14:49:58 -0600

    Lesson 2.1 - Player Positioning (Create With Code)
    1. Introduction
    - Add a player, food, animals and the ability to move left and right
    - We will add bounds through an if statement so the player can not leave the screen
    
    2. Create a new Project for Prototype 2
    - Created the Prototype 2 Unity Project
    - Imported the asset package from the Asset Store
    - Deleted SampleScene and Opened the Prototype 2 Scene
    
    3. Add the Player, Animals, and Food
    - Changed View to top down
    - Added farmer (player) to scene
    - Added all eight farm animals
    - Added bananas as the food of choice
    - Re-positioned all of the animals
    - Scaled the food up x3
    - Renamed the player to Player
    
    4. Get the user’s horizontal input
    - Added scripts folder
    - Created the PlayerController C# Script
    - Added the PlayerController script as a component of player
    - Created an horizontal input variable by the use of
    horizontalInput = Input.GetAxis("Horizontal"); 
    AND
    public float horizontalInput;
    
    5. Move the player left-to-right
    - Created a speed variable which is a float and set it to ten
    - Added a  transform.Translate line and plugged in the values so it goes back and forth
    transform.Translate(horizontalInput * Time.deltaTime * speed * Vector3.right);
    
    6. Keep the player in bounds
    - With the use of an if statement we keep the player on the screen by checking if a player is over a threshold and setting the position back to their threshold.
    
    7. Clean up your code and variables
    - Created a variable to store the absolute value maximum x threshold from the origin.
    - Added some comments
    - Changed my static -10 for the new xRange
    - Added code that makes the player not go off the right side of the screen through another if statement

---
+++ create-with-code/Unit 2 - Basic Gameplay/Prototype-2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //These two if statements keep the item inbounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(horizontalInput * Time.deltaTime * speed * Vector3.right);
+    }
+}
