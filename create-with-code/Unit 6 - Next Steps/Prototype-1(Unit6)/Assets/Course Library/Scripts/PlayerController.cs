aa144a5f34161b9184dde8020f77daeefff3c7d4
Christopher <10642047@my.uvu.edu>
2020-10-28 17:50:31 -0600

    Lesson 6.2 - Research and Troubleshooting (Create With Code)
    1. Make the vehicle use forces
    - Made a horsepower variable
    - Changed the forward force from a transform.Translate to an AddRelativeForce
    
    2. Prevent car from flipping over
    - Added a center of mass empty
    - Referenced the center of mass in the car in the player controller script
    
    3. Add a speedometer display
    - Using the TextMeshPro UI option we added a speedometer
    - Using the velocity.magnitude we were able to calculate a speed estimate and we used the 2.237 scaler to convert the speed to miles per hour
    - Once this was done I set the text to concatenate with a string and display on the UI
    
    4. Add an RPM display
    - Similar to the speed we used a TextMeshPro element to display the RPM value
    - We calculated the rpm just by using the speed with a modulo operator to make a fake rpm value
    
    5. Prevent driving in mid-air
    - Implemented the use of wheelColliders and wrote logic code to see if the car was on the ground at a given time with a Boolean value
    - If there were four wheels on the ground, then the car could move forward

--- create-with-code/Unit 6 - Next Steps/Prototype-1(Unit6)/Assets/Course Library/Scripts/PlayerController.cs
+++ create-with-code/Unit 6 - Next Steps/Prototype-1(Unit6)/Assets/Course Library/Scripts/PlayerController.cs
@@ -1,24 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
     //Private Variables
-    [SerializeField] protected const float turnSpeed = 30.0f;
-    [SerializeField] private readonly float speed = 25.0f;
-    [SerializeField] private float horizontalInput;
-    [SerializeField] private float forwardInput;
+    private float turnSpeed = 25.0f;
+    [SerializeField]private float speed = 25.0f;
+    [SerializeField] private float horsePower = 0.0f;
+    private float horizontalInput;
+    private float verticalInput;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+    public float rpm;
+    public List<WheelCollider> allWheels;
+    public int wheelsOnGround;
 
-    // Update is called once per frame
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
+
+
     void FixedUpdate()
     {
         //This gets the player input
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Vertical");
         //This is the forward code
-        transform.Translate(Time.deltaTime * speed * forwardInput * Vector3.forward);
+        //transform.Translate(Time.deltaTime * speed * verticalInput * Vector3.forward);
         //This is the turning code
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (IsOnGround())
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+            playerRb.AddRelativeForce(horsePower * verticalInput*Vector3.forward);
+
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f);
+            speedometerText.SetText("Speed: "+ speed + " MPH");
+            rpm = (speed % 30)*40;
+            rpmText.SetText("RPM: "+rpm);
+        }
+
+    }
+
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
