e804f89f1b2bb6013410adce60934768a18b42c7
Christopher <10642047@my.uvu.edu>
2020-10-28 13:36:25 -0600

    Lesson 5.1 - Clicky Mouse (Create With Code)
    1. Introduction
    - Objects will be tossed in the air
    - We will have the user click on the objects to be destroyed
    - We will give object three randomized variables
    
    2. Create project and switch to 2D view
    - Created the Prototype 5 Unity Project
    - Imported the Prototype 5 Asset pack from the Unity Store
    - Opened the Prototype 5 scene and got rid of the sample scene
    - There are different backgrounds available
    - We set the background to the 2D view instead of 3D
    
    3. Create good and bad targets
    - I chose three boxes as good targets to click and one skull as a bad objects
    - I renamed all of the objects to more logical names
    - We added box colliders and rigidbody components to all for of the objects that will be tossed up
    - Then I created a scripts folder and the target script that also got put on the clickable objects
    - Make all of the targets prefabs in the new prefab folder and deleted from the hierarchy
    
    4. Toss objects randomly in the air
    - Created an upward force in the script on the rigidbody
    - Using the random range I randomized ho high they boxes fly as well as the torque
    - Objects with the script will not jump up with different levels of randomness
    
    5. Replace messy code with new methods
    - Created three new functions that return the random values
    - Replaced the function names where they are getting called
    - Added some variables at the top for cleanliness
    
    6. Create object list in Game Manager
    - Created a new Game Manager empty
    - Created the new GameManager C# script
    - Made a list on the game manager and manually added the four elements that need to be tossed up
    
    7. Create a co-routine to spawn objects
    - Added a IEnumerator script that spawns a target
    - Added random functionality to change the targets it selects to toss
    - Added a StartCoroutine in the start function to run the spawn target
    
    8. Destroy target with click and sensor
    - Changed the OnMouseDown function to destroy a box or skull if clicked
    - Changed the OnTriggerEnter function to Destroy the tossed object it it hits a certain predetermined sensor

---
+++ create-with-code/Unit 5 - User Interface/Prototype-5/Assets/Scripts/Target.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Target : MonoBehaviour
+{
+
+    private Rigidbody targetRb;
+
+    private float minSpeed = 12.0f;
+    private float maxSpeed = 16.0f;
+    private float maxTorque = 10.0f;
+    private float xRange = 4.0f;
+    private float ySpawnpos = -2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse);
+        
+        transform.position = RandomSpawnPos() ;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnpos);
+    }
+}
