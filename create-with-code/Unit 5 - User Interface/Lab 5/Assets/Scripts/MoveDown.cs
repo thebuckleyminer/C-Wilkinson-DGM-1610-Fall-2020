64fcf8ff343aa51209a58cb04b537c8ad97223db
Christopher <10642047@my.uvu.edu>
2020-10-28 16:17:26 -0600

    Lab 5 - Swap out your Assets (Create With Code)
    - Added the lab 4 project folder to unit 5 and renamed it to lab 5
    - Imported the: Create with Code | Complete Course Library from the Unity Store
    - Imported some boxes from the asset store
    - Switched the player sphere with a fighter jet
    - Replaced the other objects with more fun objects
    - Changed the texture and tiling of the background plane and walls

---
+++ create-with-code/Unit 5 - User Interface/Lab 5/Assets/Scripts/MoveDown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDown : MonoBehaviour
+{
+    public float moveDownSpeed = 5.0f;
+    public float zDestroy = -10.0f;
+    private Rigidbody objectRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectRb.AddForce(Vector3.forward * - moveDownSpeed);
+        if (transform.position.z < zDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ create-with-code/Unit 5 - User Interface/Lab 5/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float verticalInput;
+    private float horozontalInput;
+    private float zBound = 9;
+    private float speed = 100.0f;
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horozontalInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(speed * verticalInput * Vector3.forward);
+        playerRb.AddForce(speed * horozontalInput * Vector3.right);
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x,transform.position.y,-zBound);//Take - off to endless scroll
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x,transform.position.y,zBound);//Add the - to endless scroll
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has colided with enemy.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Hit");
+        }
+        
+               
+        
+    }
+}
