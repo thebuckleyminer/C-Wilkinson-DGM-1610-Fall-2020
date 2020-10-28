42f193cd161d92b225703d3fc9c5fccf57e44194
Christopher <10642047@my.uvu.edu>
2020-10-28 09:33:59 -0600

    Lesson 4.3 - PowerUp and CountDown (Create With Code)
    1. Introduction
    - I will introduce a power-up in this module
    - I will also add a timer
    
    2. Choose and prepare a power-up
    - Added the power-up lightning bolt to the scene and made it bigger
    - Added a box collider to the power-up and made it a trigger
    - Created and applied a power-up tag
    - Made our new power-up a prefab
    
    3. Destroy power-upon collision
    - Added our own OnTriggerEnter colider method 
    - Wrote code to destroy the power-up if the player touches the power-up
    
    4. Test for enemy and power-up
    - Made the OnCollisionEnter function for the collision
    - Added and applied the enemy tag to our enemy
    - Made a Debug.Log message to output when the player hits an enemy while the power-up is active
    
    5. Apply extra knock-back with power-up
    - Added an Impulse force to send the enemy flying on contact
    
    6. Create Countdown Routine for power-up
    - Created a co-routine to time how long the player will have the power-up
    - I used the WaitForSeconds for a seven second timer
    - I use the StartCoroutine function to call the timer function
    
    7. Add a power-up indicator
    - Added a power-up ring around the player on contact with the power-up
    - Wrote code for the power-up to track the player
    - Made the default state of the power is unchecked/ not active
    - Then I wrote code to set it as active on collision with the power-up item

--- create-with-code/Unit 4 - Gameplay Mechanics/Prototype-4/Assets/Course Library/Scripts/PlayerController.cs
+++ create-with-code/Unit 4 - Gameplay Mechanics/Prototype-4/Assets/Course Library/Scripts/PlayerController.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float powerupStrength = 15.0f;
     private Rigidbody playerRb;
     public float speed = 5.0f;
 
     private GameObject focalPoint;
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
     
     // Start is called before the first frame update
     void Start()
@@ -21,5 +25,37 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(forwardInput * speed * focalPoint.transform.forward);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+            ;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer*powerupStrength, ForceMode.Impulse);
+        }
     }
 }
