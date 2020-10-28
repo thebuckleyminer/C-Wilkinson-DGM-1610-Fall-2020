60ddffb8c1aa1b23a352880336c9765c73d3ea33
Christopher <10642047@my.uvu.edu>
2020-10-28 10:13:50 -0600

    Lesson 4.4 - For-Loops For Waves (Create With Code)
    1. Introduction
    - We are going to make it so more enemies appear when knocked off
    - We are going to use a for loop to do this
    
    2. Write a for-loop to spawn 3 enemies
    - Made our own SpawnEnemyWave function to make an enemy wave when called
    - Moved the code in the start function into a loop and then into the SpawnEnemyWave SpawnEnemyWave function
    - Added a call to the SpawnEnemyWave in the start function
    
    3. Give the for-loop a parameter
    - Added a variable as a parameter to out SpawnEnemyWave as an integer that will have the value of how many enemies to spawn
    - Replaced the enimiesToSpawn parameter inside the SpawnEnemyWave function
    
    4. Destroy enemies if they fall off
    - In the Enemy script we added an if block to destroy the enemy if it has fallen ten meters off the platform
    - Added code to spawn another enemy if the number of enemies reaches zero
    
    5. Increase enemyCount with waves
    - Created a wave number variable
    - Changed the number to spawn with the waveNumber Variable
    - Increased the wavenumber by one right before spawning a new wave
    
    6. Spawn Powerups with new waves
    - implemented code to generate a new powerup with each additional enemy wave generated
    - I also added an offset because it the powerups were a little low and intersecting with the island plane

--- create-with-code/Unit 4 - Gameplay Mechanics/Prototype-4/Assets/Course Library/Scripts/SpawnManager.cs
+++ create-with-code/Unit 4 - Gameplay Mechanics/Prototype-4/Assets/Course Library/Scripts/SpawnManager.cs
@@ -1,24 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
-
+    public GameObject powerupPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
 
+    public int waveNumber = 1;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
     private Vector3 GenerateSpawnPosition()
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        Vector3 randomPos = new Vector3(spawnPosX, 0.45f, spawnPosZ);
         return randomPos;
+    }
+
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
 }
