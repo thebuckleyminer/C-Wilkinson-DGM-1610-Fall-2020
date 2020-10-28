8b61162d18013ad2d69c98bb56b54ce2d5e082ed
Christopher <10642047@my.uvu.edu>
2020-10-28 11:36:28 -0600

    Challenge 4 - Soccer Scripting (Create With Code)
    - Created the Challenge 4 Unity Game Project
    - Imported the Challenge 4 Asset pack from the Unity Store
    - Deleted the default scene
    - Fixed that hitting a enemy sent it back towards you
    - Fixed that when a new wave spawns when the player gets a powerup
    - Fixed that the powerups never went away
    - Fixed that the 2 enemies are spawned in every wave
    - Fixed that the enemy balls are not moving anywhere
    - Made the space-bar make the player jump and emit a particle system
    - Fixed that the enemies never got more difficult

---
+++ create-with-code/Unit 4 - Gameplay Mechanics/Challenge 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    private SpawnManagerX spawnMan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+        spawnMan = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+        speed = spawnMan.waveCount * 25;
+        Debug.Log(spawnMan.waveCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(speed * Time.deltaTime * lookDirection);
+        
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+            
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+            ;
+        }
+
+    }
+
+}
