5289dfa0ffcbddcb2bb719a21fa9c91f761d6f2c
Christopher <10642047@my.uvu.edu>
2020-10-27 21:59:08 -0600

    Lab 3 - Player Control (Create With Code)
    - Copied the Lab2 folder to Unit 3 and changed the name from Lab 2 to Lab3
    - Added a rigidbody to the player
    - Added gravity to the rigidbody
    - Created a scripts folder
    - Created my PlayerController script
    - Placed the script on the player
    - Added movement through the GetAxis on the horizontal and Vertical Axis
    - Added walls and virtual boundaries so the player would stay on the screen
    - Added a purple materiel for the walls
    - Added environmental objects to an environment object
    - Tidied up the code a little bit
    - Lastly I exported a backup like it instructed me to do

---
+++ create-with-code/Unit 3 - Sound and Effects/Lab 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float verticalInput;
+    private float horozontalInput;
+    private float zBound = 9;
+    private float speed = 5.0f;
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horozontalInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(speed * verticalInput * Vector3.forward);
+        playerRb.AddForce(speed * horozontalInput * Vector3.right);
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x,transform.position.y,-zBound);//Take - off to endless scroll
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x,transform.position.y,zBound);//Add the - to endless scroll
+        }
+    }
+}
