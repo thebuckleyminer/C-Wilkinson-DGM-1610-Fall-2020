3df54ca080acae1a2b0a74e421958c99c83b6771
Christopher <10642047@my.uvu.edu>
2020-11-06 20:09:12 -0700

    October 8th (DGM-1610 Class Meeting)
    Calendar items for today:
        - Scriptable objects, floats, bools, and ints.
        - Create a health bar.
    --------------------------------------------------------------------------------------------------------------------------
    --------------------------------------------------------------------------------------------------------------------------
    Summary of what I accomplished in this Class Meeting:
        - BoolData script and folder.
        - FloatData script and folder.
        - PlayerHealth float Scriptable object.
        - Created UI image for the health bar.
        - Made sprites folder and square square sprite.
        - Created a health bar.
        - Added the line using UnityEngine.UI.
        - DisplayValue(Image img).
        - img.fillAmount = value.
        - Created the image behavior script to make the health bar work.
    --------------------------------------------------------------------------------------
    --------------------------------------------------------------------------------------
    Key items Romrell taught us today:
        - Talked about ADHD, and how he uses it as an amazing superpower.
        - Told us to talk to him about a plan of action if we are behind.
        - Talked about how the MoveLeft script and PlayerController script from Create with Code are badly written scripts.
        - Reviewed what we have previously learned in class.
        - Scriptable objects are basically just global variables.
        - Why scriptable objects? If we need to know if the player has good health, and a health bar, and a powerup needs to know about the health, and unhealthy cake. An enemy might also need to know about your health.
        - All of these things need to hypothetically know about the health, so this is where a scriptable object comes in.
        - A trigger is when one object intercets or passes through another object.
        - A scriptable object is a variable everything in your game can see.
        - += is the same is x = x + y.
        - We learned about how to make a health bar out of a scriptable object.

---
+++ class-unity-projects/Cyborg Shrimp/Assets/ScriptableObjects/Float/FloatData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float value;
+
+    public void UpdateValue(float number)
+    {
+        value += number;
+    }
+}
---
+++ class-unity-projects/Cyborg Shrimp/Assets/Scripts/ImageBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class ImageBehavior : MonoBehaviour
+{
+    private Image img;
+
+    private void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    public void DisplayValue(FloatData data)
+    {
+        img.fillAmount = data.value;
+    }
+}
