8322084ba403854c4eaa0491af843c44bb35a3f3
Christopher <10642047@my.uvu.edu>
2020-11-11 17:21:24 -0700

    October 29th (DGM-1610 Class Meeting)
    Summary of what I accomplished in this Class Meeting:
        - Money Scriptiable Int Object.
        - Cash.
        - Set active to false with code.
        - reworked how the sprite in on the screen for this demo.
        - Display Image.
        - We then took off and made a text UI to display the money value and much much more.
    --------------------------------------------------------------------------------------
    --------------------------------------------------------------------------------------
    Key items Romrell taught us today:
        - A code has separate components and they all do different things.
        - In the programming world they call these things objects. They call it Object Oriented Programming.
        - In Object Oriented Programming they make these little classes or a script.
        - The instances of these classes are the objects.
        - They are nice because they are small and only do a few things.
        - We use the Transform Object all the time... also called component.
        - Learned a lot about Scriptiable Objects.
        - Learned how to share data easier.

---
+++ class-unity-projects/Cyborg Shrimp/Assets/Scripts/CollectCash.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectCash : MonoBehaviour
+{
+    public IntData cashTotal;
+    public CollectibleItem collectibleObj;
+    public Text txtObj;
+    private SpriteRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = collectibleObj.art;
+        renderer.color = collectibleObj.artColor;
+        txtObj.text = cashTotal.value.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        cashTotal.value += collectibleObj.cashValue;
+        txtObj.text = cashTotal.value.ToString();
+        gameObject.SetActive(false);
+    }
+    
+    
+    
+    
+}
--- class-unity-projects/Cyborg Shrimp/Assets/Scripts/CollectibleItem.cs
+++ class-unity-projects/Cyborg Shrimp/Assets/Scripts/CollectibleItem.cs
@@ -3,7 +3,7 @@
 [CreateAssetMenu]
 public class CollectibleItem : ScriptableObject
 {
-    public float powerLevel = 1f;
+    //public float powerLevel = 1f;
     public int cashValue = 10;
     public Sprite art;
     public Color artColor = Color.blue;
