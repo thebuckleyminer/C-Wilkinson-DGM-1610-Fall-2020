09df3083cf1c96dff0d814ada336cbe96071b57b
Christopher <10642047@my.uvu.edu>
2020-11-13 07:24:15 -0700

    November 5th (DGM-1610 Class Meeting)
    Summary of what I accomplished in this Class Meeting:
        - Game Health Bar change from events.
        - Made a Game Over Screen.
        - Made a game restart button and functionality.
    --------------------------------------------------------------------------------------
    --------------------------------------------------------------------------------------
    Key items Romrell taught us today:
        - How to use events, actions, calls, and handlers to add functionality.
        - How to refactor code to use calls and handlers to implement a health bar, game over, and restart system.

--- class-unity-projects/Cyborg Shrimp/Assets/ScriptableObjects/Float/FloatData.cs
+++ class-unity-projects/Cyborg Shrimp/Assets/ScriptableObjects/Float/FloatData.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class FloatData : ScriptableObject
 {
     public float value;
-
+    public UnityEvent onZeroEvent;
+    
     public void UpdateValue(float number)
     {
         value += number;
@@ -13,6 +15,16 @@ public class FloatData : ScriptableObject
 
     public void DiplayImage(Image img)
     {
+        if (value <= 0)
+        {
+            onZeroEvent.Invoke();
+        }
+
+        if (value >= 1)
+        {
+            value = 1;
+        }
+        
         img.fillAmount = value;
     }
 
