deac3dc35ca0b648b79bbe0625ef6e18477b21d3
Christopher <10642047@my.uvu.edu>
2020-11-19 17:58:48 -0700

    November 19th (DGM-1610 Class Meeting)
    Summary of what I accomplished in this Class Meeting /Key items Romrell taught us today:
        - To move game parts to other computers or projects you export it as a package.
        - Co-routines.
        - While Loops.
        - Void functions run all at once.
        - A function could be called a routine, a coroutine is something that is run besides it.
        - Made the AddForceBehavior C# Script.
        - Made a rigidbody jump with an IEnumerator with WaitForSeconds.
        - Used a while loop to have the block jump a given number of times.
        - Co-routines can run now, hold, repeat, and stop.
        - Update Runs, Repeats, and there are no stops.
        - Made the CoroutineEvents Script.

---
+++ class-unity-projects/Cyborg Shrimp/Assets/Scripts/AddForceBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class AddForceBehavior : MonoBehaviour
+{
+    private Rigidbody rigid;
+    public Vector3 forces;
+    public float holdTime = 1.0f;
+    public int counter = 10;
+
+    private IEnumerator Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(holdTime);
+            rigid.AddForce(forces);
+            counter--;
+        }
+
+    }
+
+}
---
+++ class-unity-projects/Cyborg Shrimp/Assets/Scripts/CoroutineEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineEvents : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float holdTime, repeatHoldTime = 0.25f;
+    public int counter =3;
+
+    public void RunCorutine()
+    {
+        StartCoroutine(Coroutine());
+    }
+    private IEnumerator Coroutine()
+    {
+        
+        startEvent.Invoke();
+        yield return new WaitForSeconds(holdTime);
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return new WaitForSeconds(holdTime);
+            counter--;
+        }
+        
+        yield return new WaitForSeconds(holdTime);
+        endEvent.Invoke();
+    }
+
+}
